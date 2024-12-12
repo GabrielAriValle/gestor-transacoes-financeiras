17e3f69c137a7bb1b9ed5fc45181ac6bcb6714b7
GabrielAriValle <gabriel.avalle@hotmail.com>
2024-12-11 22:51:07 -0300

    initial tests

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
+PROJECT_NAME := gestor-transacoes
+PYTHON_VERSION := 3.11.2
+
+VENV_NAME := $(PROJECT_NAME)-$(PYTHON_VERSION)
+
+create-venv:
+	pyenv install $(PYTHON_VERSION) -v
+	pyenv virtualenv $(PYTHON_VERSION) $(VENV_NAME)
+	pyenv local $(VENV_NAME)
+	pip install --upgrade pip
+	pip install -r requirements.txt
+
 migrations:
 	python3 manage.py makemigrations
 
--- gestor_transacoes/tests.py
+++ gestor_transacoes/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+
+
+def test_should_register_cliente(cliente):
+    # GIVEN
+    data = {
+        'nome': 'teste',
+        'cpf': '000000000000',
+        'email': 'teste@teste.com',
+        'telefone': '48987654321'
+    }
+    
+    # WHEN
+    response = cliente.post('/clientes', data)
+    
+    # THEN
+    assert response.status == 200
+
+
+def test_should_register_transacao(transacao):
+    # GIVEN
+    data = {
+        ''
+    }
+    
+    
+class TestsCLientTransaction(APITestCase):
+    def setUp(self):
+        url_base = '/clients'
+        
+        return super().setUp()
+    
+    def test_should_register_client(self, client):
+        # GIVEN
+        data = {'cpf': '00000000a000'}
+        
+        # WHEN
+        response = client.post(self.url_base, data)
+        
+        # THEN
+        assert response.status == 200
